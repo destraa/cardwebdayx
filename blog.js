949cdfc3908cb077a99cd8eb4cdbe3a90cfdf06d
Destra <dewans047@gmail.com>
2023-10-04 02:33:33 +0700

    menambah blog-detail

--- blog.js
+++ blog.js
@@ -1,143 +1,338 @@
- const myProject = [];
+let dataBlog = [];
 
-// Hitung Durasi pengerjaan Project
-let days="";
-let months="";
-function durationCount (start, end){
+function addBlog(event) {
+  event.preventDefault();
 
-    let dateStart = new Date (start);
-    let dateEnd = new Date (end);
-    let oneDay = (1000 * 3600 * 24);
+  let title = document.getElementById("input-blog-title").value;
+  let content = document.getElementById("input-content-blog").value;
+  let image = document.getElementById("input-blog-image").files;
 
-    let selisih = dateEnd.getTime() - dateStart.getTime();
-    let daysTotal = selisih/oneDay;
-    let inAmonth = Math.floor(daysTotal/30);
-        daysTotal = daysTotal % 30;
+  // icon
+  const iconNodeJs = `<i class="fa-brands fa-node-js fa-xl"></i>`;
+  const iconReactJs = `<i class="fa-brands fa-react fa-xl"></i>`;
+  const iconJavaScript = `<i class="fa-brands fa-js fa-xl"></i>`;
+  const iconVueJs = `<i class="fa-brands fa-vuejs fa-xl"></i>`;
 
-    days = daysTotal;
-    months = inAmonth;
+  //check kondisi
 
-    console.log(selisih);
-    console.log(daysTotal);
-    console.log(inAmonth);
-   
+  let checkNodeJS = document.getElementById("check-nodejs").checked
+    ? iconNodeJs
+    : "";
+  let checkReactJS = document.getElementById("check-reactjs").checked
+    ? iconReactJs
+    : "";
+  let checkJavaScript = document.getElementById("check-javascript").checked
+    ? iconJavaScript
+    : "";
+  let checkVueJs = document.getElementById("check-vuejs").checked
+    ? iconVueJs
+    : "";
+
+  // durasi
+
+  let startDate = document.getElementById("input-start-date").value;
+  let endDate = document.getElementById("input-end-date").value;
+
+  let startDateValue = new Date(startDate);
+  let endDateValue = new Date(endDate);
+
+  let rentangWaktu = endDateValue.getTime() - startDateValue.getTime();
+  let rentangHari = rentangWaktu / (1000 * 3600 * 24);
+  let rentangMinggu = Math.floor(rentangHari / 7);
+  let rentangBulan = Math.floor(rentangHari / 30);
+  let rentangProject = "";
+
+  if (rentangHari <= 6) {
+    rentangProject = rentangHari + " One Day ";
+  } else if (rentangMinggu <= 3) {
+    rentangProject = rentangMinggu + " One Week ";
+  } else if (rentangBulan >= 1) {
+    rentangProject = rentangBulan + " One Month ";
+  }
+
+  image = URL.createObjectURL(image[0]);
+  // console.log(image);
+
+  let blog = {
+    title,
+    content,
+    image,
+    checkNodeJS,
+    checkJavaScript,
+    checkReactJS,
+    checkVueJs,
+    rentangProject,
+  };
+  // alert
+  if (title == "" || content == "" || image == "") {
+    return Swal.fire({
+      icon: "warning",
+      title: "Beboop!...",
+      text: "Harap lengkapi form!",
+      confirmButtonColor: "#930e2d",
+      iconColor: "#930e2d",
+    });
+  }
+
+  dataBlog.push(blog);
+  console.log(dataBlog);
+
+  renderBlog();
 }
 
+// ===================================================================================================================================================================================================================================================================================================================
 
+function renderBlog() {
+  document.getElementById("form-project-card-add").innerHTML = "";
 
-function addMyProject(event){
-    event.preventDefault();
+  for (let index = 0; index < dataBlog.length; index++) {
+    console.log(dataBlog[index]);
 
-    
-    let projectName = document.getElementById("nama-project").value;
-    let startDate = document.getElementById("start-date").value;
-    let endDate = document.getElementById("end-date").value;
-    let descProject = document.getElementById("desc-project").value;
-
-    // Ambil Value Checkbox yang Checked.
-    let checkbox = document.getElementsByName("tech");
-    let tech=[];
-      for (let i = 0; i < checkbox.length; i++){
-        if (checkbox[i].checked){
-          let techOption = checkbox[i].value;
-            // console.log(techoption);
-            tech.push(techOption);   
-        }  
-      };
-     let imgProject = document.getElementById("input-blog-image").files;
-    // script untuk mengubah file img jadi url
-     imgProject = URL.createObjectURL(imgProject[0]);
-
-    // console.log(imgProject)
-
-    //Hitung durasi pengerjaan
-    durationCount(startDate,endDate);
-
-       
-
-const myProjectObject = {
-    projectName,
-    days,
-    months,
-    techProject:tech,
-    descProject,
-    imgProject
-}
-
-    myProject.push(myProjectObject);
-     console.log(myProject);
-    renderMyProject ();
-
-};
-
-
-
- function renderMyProject (){
-    let myProjectHTML = "";
-
-    for(let i=0; i< myProject.length; i++){
-        let project = myProject[i];
-
-        // looping menampilkan img of tech
-        let techProject="";
-            for (let i =0; i < project.techProject.length; i++ ){
-                techProject += `
-                <img src="./img/tech/${project.techProject[i]}.svg"></img>
-                `;
-            };
-        // kondisi untuk menampilkan durasi
-        let duration="";
-            if(project.months === 0){
-                duration +=`
-                ${project.days} Days
-                `
-            } else if (project.months === 1){
-                duration += `
-                ${project.months} Month ${project.days} Days
-                `
-            } else{
-                duration += `
-                ${project.months} Months ${project.days} Days
-                `
-            }
-                
-
-        myProjectHTML += `
-                    <a href="01-project.html" target="blank">
-                    <div class="card-project-1">
-                    <div class="container-card-project">
-                        <div class="card-project-img">
-                            <img src="${project.imgProject}">
-                        </div>
-                        <div class="card-project-title">${project.projectName}</div>
-                        <div class="card-project-duration">
-                            <div>
-                            Duration :${duration}
-                            </div>
-                        </div>
-                        <div class="card-project-desc">
-                            <p>
-                               ${project.descProject}
-                            </p>
-                        </div>
-
-                        <div class="card-project-tech">
-                        <div class="tech-option">
-                        ${techProject}
-                        </div>
-                        </div>
-                         
-                        <div class="card-project-button">
-                        <button class="edit-button">Edit</button>
-                        <button class="delete-button">Delete</button>
-                        </div>
-                           
+    document.getElementById("form-project-card-add").innerHTML += `
+    <div class="form-project-card">
+                <div class="form-project-image">
+                    <img id="image-add" class="image-add" src="${dataBlog[index].image}"/>
+                </div>
+                <div class="title-add-project">
+                    <h3><a href="blog-detail.html">${dataBlog[index].title}</a></h3>
+                </div>
+                <div class="duration-project">
+                    <p>Duration : ${dataBlog[index].rentangProject}</p>
+                </div>
+                <div class="description-project">
+                    <p>${dataBlog[index].content}</p>
+                </div>
+                <div class="technologies-project">
+                    <div class="technologies-project-card">
+                        ${dataBlog[index].checkNodeJS}
+                        ${dataBlog[index].checkJavaScript}
+                        ${dataBlog[index].checkReactJS}
+                        ${dataBlog[index].checkVueJs}
                     </div>
-                </div></a> `;
-       
-    };
+                </div>
+                <div class="button-add-project">
+                    <button type="button" class="edit-button" id="edit-button">Edit</button>
+                    <button type="button" class="delete-button" id="delete-button">Delete</button>
+                </div>
+            </div>`;
+  }
+}
 
-    document.querySelector(".container-content").innerHTML = myProjectHTML;
-};
+function dummyCard() {
+  document.getElementById("form-project-card-add").innerHTML = "";
 
+  // card 1
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >Lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-node-js fa-xl"></i>
+                <i class="fa-brands fa-js fa-xl"></i>
+                <i class="fa-brands fa-react fa-xl"></i>
+                <i class="fa-brands fa-vuejs fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
 
+  // card 2
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >Lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-node-js fa-xl"></i>
+                <i class="fa-brands fa-js fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
+
+  // card 3
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-node-js fa-xl"></i>
+                <i class="fa-brands fa-react fa-xl"></i>
+                <i class="fa-brands fa-vuejs fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
+
+  // card 4
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >Lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-js fa-xl"></i>
+                <i class="fa-brands fa-vuejs fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
+
+  // card 5
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >Lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-react fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
+
+  // card 6
+  for (let i = 0; i < 1; i++) {
+    document.getElementById("form-project-card-add").innerHTML += `
+        <div class="form-project-card">
+        <div class="form-project-image">
+            <img id="image-add" class="image-add" src="./image-repository/martadinataclass.jpg"/>
+        </div>
+        <div class="title-add-project">
+        <h3>
+          <a href="blog-detail.html" 
+            >Lorem Ipsum
+          </a>
+        </h3>
+      </div>
+        <div class="duration-project">
+            <p>Duration : 3 Month</p>
+        </div>
+        <div class="description-project">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium dolores ullam minima reiciendis adipisci quisquam</p>
+        </div>
+        <div class="technologies-project">
+            <div class="technologies-project-card">
+                <i class="fa-brands fa-js fa-xl"></i>
+                <i class="fa-brands fa-react fa-xl"></i>
+                <i class="fa-brands fa-vuejs fa-xl"></i>
+            </div>
+        </div>
+        <div class="button-add-project">
+            <button type="button" class="edit-button" id="edit-button">Edit</button>
+            <button type="button" class="delete-button" id="delete-button">Delete</button>
+        </div>
+         </div>
+        `;
+  }
+}
+
+dummyCard();
